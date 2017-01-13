8d5259331fdbab3813adf2aef4ce13102b951a95
sachint147 <tiwarisachin147@gmail.com>
2017-01-13 22:46:23 +0530

    solution completed

--- src/hackerrank/datastructure/DynamicArray.java
+++ src/hackerrank/datastructure/DynamicArray.java
@@ -1,41 +1,40 @@
 package hackerrank.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class DynamicArray {
 
 	public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int Q = sc.nextInt();
-        
-        List<Integer>[] seqList = new ArrayList[N];
-        
-        for(int i=0; i<N; i++) {
-            seqList[i] = new ArrayList<>();
-        }
-        
-        int lastAns = 0;
-        
-        for(int i=0; i<Q; i++) {
-            int queryType = sc.nextInt();
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            
-            if(queryType == 1) {
-                operateQueryOne(seqList);
-            } else {
-                operateQueryTwo();          
-            }
-        }
-    }
-    
-    
-    public static void operateQueryOne(seqList) {
-        
-    }
-    
-    public static void operateQueryTwo(seqList) {
-        
-    }
+		/*
+		 * Enter your code here. Read input from STDIN. Print output to STDOUT.
+		 * Your class should be named Solution.
+		 */
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int Q = sc.nextInt();
+
+		List<ArrayList<Integer>> seqList = new ArrayList<ArrayList<Integer>>();
+
+		for (int i = 0; i < N; i++) {
+			seqList.add(new ArrayList<>());
+		}
+
+		int lastAns = 0;
+
+		for (int i = 0; i < Q; i++) {
+			int queryType = sc.nextInt();
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			List<Integer> sequence = seqList.get((x ^ lastAns) % N);
+			if (queryType == 1) {
+				sequence.add(y);
+			} else {
+				lastAns = sequence.get(y % sequence.size());
+				System.out.println(lastAns);
+			}
+		}
+	}
 }
