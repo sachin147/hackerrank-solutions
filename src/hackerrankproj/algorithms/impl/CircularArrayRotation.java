201b5838627a94003e103d9944d65f8da5a6d32c
sachint147 <tiwarisachin147@gmail.com>
2017-02-02 00:13:05 +0530

    solution

--- src/hackerrankproj/algorithms/impl/CircularArrayRotation.java
+++ src/hackerrankproj/algorithms/impl/CircularArrayRotation.java
@@ -2,7 +2,31 @@ package hackerrankproj.algorithms.impl;
 
 public class CircularArrayRotation {
 
-	public static void main(String args[]) {
-		
-	}
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int q = sc.nextInt();
+        int []a = new int[n];
+        int []position = new int[n];
+        
+        for(int i=0; i < n; i++){
+            a[i] = sc.nextInt();
+            position[i] = sc.nextInt();
+        }
+        for(int i = 0; i < n; i++){
+            position[i] = (position[i] + k)%n;
+        }
+        
+        
+        for(int i=0; i<q;i++) {
+            int m = sc.nextInt();
+            for(int j=0; j<n;j++) {
+                if(position[j]== m){
+                    System.out.println(position[j]);
+                    break;
+                }
+            }
+        }
+    }
 }
